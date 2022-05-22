74cf02444b63e5c28786b2709dec19c5c1325107
faijan <farman0000000786@gmail.com>
2022-05-22 16:46:19 +0530

    produc api

---
+++ src/api/sData.js
@@ -0,0 +1,28 @@
+const sData = [
+    {
+        cardimg: "/images/card-img.png",
+        curprice: "150",
+        realprice: "500",
+        cardCon: "Men's Blue One With the Universe Graphic Printed T-shirt"
+    },
+    {
+        cardimg: "/images/card-img.png",
+        curprice: "350",
+        realprice: "600",
+        cardCon: "Men's Blue One With the Universe Graphic Printed T-shirt"
+    },
+    {
+        cardimg: "/images/card-img.png",
+        curprice: "250",
+        realprice: "500",
+        cardCon: "Men's Blue One With the Universe Graphic Printed T-shirt"
+    },
+    {
+        cardimg: "/images/card-img.png",
+        curprice: "350",
+        realprice: "400",
+        cardCon: "Men's Blue One With the Universe Graphic Printed T-shirt"
+    }
+]
+
+export default sData;
--- src/components/FirstSec.jsx
+++ src/components/FirstSec.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import sData from '../api/sData';
 
 const FirstSec = () => {
   return (
@@ -10,10 +11,10 @@ const FirstSec = () => {
     </div>
       <section className="container-fluid cards-section">
         <div className="row d-flex gap-4 align-item-center justify-content-center">
-            <Card cardimg='/images/card-img.png' cardP='sdf' curprice='350' realprice='500' cardCon="Men's Blue One With the Universe Graphic Printed T-shirt"/>
-            <Card cardimg='/images/card-img.png' cardP='sdf' curprice='350' realprice='500' cardCon="Men's Blue One With the "/>
-            <Card cardimg='/images/card-img.png' cardP='sdf' curprice='350' realprice='500' cardCon="Men's Blue One With the Universe Graphic Printed T-shirt"/>
-            <Card cardimg='/images/card-img.png' cardP='sdf' curprice='350' realprice='500' cardCon="Men's Blue One With the "/> 
+            <Card cardimg={sData[0].cardimg} curprice={sData[0].curprice} realprice={sData[0].realprice}  cardCon={sData[0].cardCon} />
+            <Card cardimg={sData[1].cardimg} curprice={sData[1].curprice} realprice={sData[1].realprice}  cardCon={sData[1].cardCon} />
+            <Card cardimg={sData[2].cardimg} curprice={sData[2].curprice} realprice={sData[2].realprice}  cardCon={sData[2].cardCon} />
+            <Card cardimg={sData[3].cardimg} curprice={sData[3].curprice} realprice={sData[3].realprice}  cardCon={sData[3].cardCon} /> 
         </div>
     </section>
     </>
